3732c69188d6ceb01fe099fd7dc990194bb9736a
Paul Ian Lim <paul.iand3138@hotmail.com>
2026-06-24 06:16:34 +0000

    data correction

--- models/station_dimension.sql
+++ models/station_dimension.sql
@@ -6,7 +6,7 @@ start_station_id as station_id,
 start_station_name as station_name,
 start_lat as station_lat,
 start_lng as start_station_lng
-from {{ source('demo', 'bike') }}
+from {{ ref('stg_bike') }}
 where ride_id != 'ride_id'
 )
 
--- models/trip_fact.sql
+++ models/trip_fact.sql
@@ -2,13 +2,13 @@ with trips as (
 
 select
 ride_id,
-rideable_type,
+-- rideable_type,
 date(to_timestamp(started_at)) as trip_date,
 start_station_id,
 end_station_id,
 member_csual as member_casual,
 timestampdiff(second, to_timestamp(started_at),  to_timestamp(ended_at)) as trip_duration_seconds
-from {{ source('demo', 'bike') }}
+from {{ ref('stg_bike') }}
 where ride_id != 'ride_id'
 limit 10
 )
